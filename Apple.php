01b9ba1e7bd9c28d394be39821cda1bb4983af9a
Keizashi <suhoffpi-88@yandex.ru>
2021-03-15 18:23:12 +0400

    adding params times and state of integrity

--- Apple.php
+++ Apple.php
@@ -2,16 +2,26 @@
 
 class Apple
 {
-    /** @var array набор цветов константа COLORS */
+
+    /** @var array Набор цветов константа */
     const COLORS = ['red', 'yellow', 'green'];
-    
-    /**  @var string color цвет яблока */
+
+    /**  @var string Цвет яблока */
     private $color;
+
+    /**  @var DateTime время появления */
+    private $timeOfAppearance;
+
+    /**  @var DateTime время падения */
+    private $timeOfFall = null;
+
+    /**  @var int целостность яблока */
+    private $integrityOfApple = 100;
 
     public function __construct()
     {
         $this->color = self::COLORS[array_rand(self::COLORS, 1)];
+        $this->timeOfAppearance = date("Y-m-d H:i:s");
     }
-
 
 }
