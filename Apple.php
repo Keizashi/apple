2e6733c30c204b52a04d13ceab7b6d9ba49b2bf3
Keizashi <suhoffpi-88@yandex.ru>
2021-03-15 19:12:09 +0400

    6-adding-params-and-states-for-apple-fixies

--- Apple.php
+++ Apple.php
@@ -6,22 +6,24 @@ class Apple
     /** @var array Набор цветов константа */
     const COLORS = ['red', 'yellow', 'green'];
 
-    /**  @var string Цвет яблока */
+    /** @var string Цвет яблока */
     private $color;
 
-    /**  @var DateTime время появления */
-    private $timeOfAppearance;
+    /** @var string время появления */
+    private $appearanceTimestamp;
 
-    /**  @var DateTime время падения */
+    /** @var string время падения */
     private $timeOfFall = null;
 
-    /**  @var int целостность яблока */
+    /** @var int целостность яблока */
     private $integrityOfApple = 100;
 
     public function __construct()
     {
         $this->color = self::COLORS[array_rand(self::COLORS, 1)];
-        $this->timeOfAppearance = date("Y-m-d H:i:s");
+
+        $date = date_create();
+        $this->appearanceTimestamp = date('m/d/Y H:i:s', date_timestamp_get($date));
     }
 
 }
